59400e7e514a04518e3cd737acfdc723f6f522cb
pelumi <polalekan@gmail.com>
2022-09-21 23:54:38 +0100

    task 101 fixed negative index case

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    newStr = str[:n] + str[n + 1:]
+    if n < 0:
+        newStr = str
+    else:
+        newStr = str[:n] + str[n + 1:]
     return (newStr)
