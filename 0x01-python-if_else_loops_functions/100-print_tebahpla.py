394dc24da71357e53f3e504d92cc5e0422537b0c
pelumi <polalekan@gmail.com>
2022-09-21 23:46:30 +0100

    task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,8 @@
 i = 0
 for alph in reversed(range(ord('a'), ord('z') + 1)):
     if i % 2 == 0:
-        print(chr(alph), end='')
+        letter = chr(alph)
     else:
-        print(chr(alph - 32), end='')
+        letter = chr(alph - 32)
+    print("{}".format(letter), end='')
     i += 1
