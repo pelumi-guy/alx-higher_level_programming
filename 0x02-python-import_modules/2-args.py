001593433dea61c18d094aa15d536431a090df1a
pelumi <polalekan@gmail.com>
2022-09-23 03:41:49 +0100

    updated task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+from sys import argv
+
 if __name__ == "__main__":
-    from sys import argv
     if len(argv) == 1:
         print("{} arguments.".format(len(argv) - 1))
     else:
