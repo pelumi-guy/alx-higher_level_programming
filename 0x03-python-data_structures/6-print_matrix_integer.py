981baa3feaae8700ff08bbbeafe8eddb7d78412d
pelumi <polalekan@gmail.com>
2022-09-26 02:11:06 +0100

    task 6 updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix[0]) > 0:
-        for row in matrix:
-            for elm in row:
-                print("{}".format(elm), end=' ' if row.index(elm) <
-                      len(row) - 1 else '\n')
-    else:
+    for row in matrix:
+        for elm in row:
+            print("{}".format(elm), end=' ' if row.index(elm) <
+                  len(row) - 1 else '')
         print()
