402d1bf7a884a160bec347f460c18ad41fea962a
pelumi <polalekan@gmail.com>
2022-09-29 05:48:56 +0100

    task 101 fixed line count

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    resMatx = []
-    resMatx = list(map(lambda x: list(map(lambda y: y*y, x)), matrix))
-    return resMatx
+    return list(map(lambda x: list(map(lambda y: y*y, x)), matrix))
