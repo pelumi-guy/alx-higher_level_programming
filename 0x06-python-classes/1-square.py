46591fece2851068e455d400ed5d989517279d61
pelumi <polalekan@gmail.com>
2022-10-12 04:29:44 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a class Square instantiated with a private size variable"""
+
+
+class Square:
+    """a square object"""
+    def __init__(self, size):
+        self.__size = size
