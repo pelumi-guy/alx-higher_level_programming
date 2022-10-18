7a0e3de2f719ccdc948aa8ecee69cb8396223a8e
pelumi <polalekan@gmail.com>
2022-10-18 02:31:28 +0100

    updated task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -16,8 +16,8 @@ class Rectangle:
             TypeError: if size is not integer
             ValueError: if size is less than zero
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         type(self).number_of_instances += 1
 
     @property
