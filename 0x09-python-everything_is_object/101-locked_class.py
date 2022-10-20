badeea7f7bd770a592e5f1494c00bedfecc8bfe5
Pelumi <pelumi.olalekan.g@gmail.com>
2022-10-20 00:56:16 +0000

    task 101 updated

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,9 +2,9 @@
 """Low memory cost"""
 
 
-class LockedClass(object):
+class LockedClass:
     """
     Only allows instatiation of an attribute called first_name
     """
 
-    __slots__ =  'first_name'
+    __slots__ =  ('first_name')
