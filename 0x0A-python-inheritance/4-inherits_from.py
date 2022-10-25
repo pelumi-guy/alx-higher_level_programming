f7431ff9b9a7b19d245c61666175efff95505000
pelumi <polalekan@gmail.com>
2022-10-25 22:27:31 +0100

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Only sub class of"""
+
+
+from xmlrpc.client import boolean
+
+
+def inherits_from(obj, a_class):
+    """a function that checks if object is an
+    instance of a given class that inherited
+    (directly or indirectly) from the specified
+    class ; otherwise False.
+
+    Args:
+        @obj: object instance
+        @a_class: class to check for
+
+    Return:
+        True if the object is an instance of inherited
+        class of the specified class; otherwise False."""
+
+    if a_class in type(obj).__mro__ and a_class != type(obj):
+        return True
+    else:
+        return False
