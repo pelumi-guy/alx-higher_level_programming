3ef075aab2ad228fcd2f2d29496b47c3ccd71400
pelumi <polalekan@gmail.com>
2022-10-25 22:50:46 +0100

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Improve Geometrye"""
+
+
+class BaseGeometry:
+    """Geometry class"""
+    def area(self):
+        """"Simple method that raise an exception"""
+        raise Exception("area() is not implemented")
