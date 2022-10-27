31806bfe2297fed17c44d78a5b5be699362a32a3
pelumi <polalekan@gmail.com>
2022-10-27 01:36:26 +0100

    task 10 and 11 updated

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """11. Student to disk and reload"""
-load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
 
 class Student:
@@ -19,16 +17,10 @@ class Student:
     def to_json(self, attrs=None):
         """function that retrieves a dictionary
         representation of a Student instance"""
-        if attrs:
-            ret_dict = {}
-            for attr in attrs:
-                try:
-                    ret_dict[attr] = getattr(self, attr)
-                except AttributeError:
-                    pass
-            return ret_dict
-        else:
-            return self.__dict__
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
     def reload_from_json(self, json):
         """method that replaces all attributes
