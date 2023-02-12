80866ee1c31fe295126002181a1148740143f06a
Pelumi <pelumi.olalekan.g@gmail.com>
2023-02-12 15:28:25 +0000

    task 0, 1 and 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request
+  .get(url)
+  .on('response', function (res) {
+    console.log(`code: ${res.statusCode}`);
+  });
